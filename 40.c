7fb8987b145f3f63106bcc11c9ec26ef146d70c1
Ften <49495951+tr0i@users.noreply.github.com>
2019-04-16 21:10:16 +0300

    new convert program

---
+++ 40.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int ConvertDecimalToOctal(int decimal);
+
+int main()
+{
+    int decimalNumber, octalNumber;
+    decimalNumber = 1;
+
+    int i;
+    for(i = 205; i < 256; ++i)
+    {
+        octalNumber = ConvertDecimalToOctal(i);
+        printf("[%d] - [%d]\n", i, octalNumber);
+    }
+
+    return 0;
+}
+
+
+int ConvertDecimalToOctal(int decimal)
+{
+    int base = 1;
+    int result = 0;
+
+    while(decimal != 0)
+    {
+        result += (decimal % 8) * base;
+        decimal /= 8;
+        base *= 10;
+    }
+
+    return result;
+}
